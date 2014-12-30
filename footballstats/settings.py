1cff886fd07d71af8ffab5290b98e8c1c06664de
Michael Brew <michael.brew@movial.com>
2014-12-30 17:27:25 -0500

    Committing initial implementation

---
+++ footballstats/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for footballstats project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'footballstats'
+
+SPIDER_MODULES = ['footballstats.spiders']
+NEWSPIDER_MODULE = 'footballstats.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'footballstats (+http://www.yourdomain.com)'
---
+++ footballstats/spiders/stats_spider.py
@@ -0,0 +1,65 @@
+import scrapy
+
+from footballstats.items import resultsItem
+from footballstats.items import fixturesItem
+from footballstats.items import ranksItem
+
+TEAM_NAME = "bolton-wanderers"
+LEAGUE_NAME = "championship"
+
+class resultsSpider(scrapy.Spider):
+    name = "results"
+    allowed_domains = ["bbc.com"]
+    start_urls = ["http://www.bbc.com/sport/football/teams/{0}/results".format(TEAM_NAME)]
+    
+    def parse(self, response):
+        for sel in response.xpath('//tr[@class="report"]'):
+            game = resultsItem()
+            game['date'] = sel.xpath('td[@class="match-date"]/text()').extract()[0]
+            game['homeTeam'] = sel.xpath('td[@class="match-details teams"]/p/span[@class="team-home teams"]/a/text()').extract()[0]
+            game['awayTeam'] = sel.xpath('td[@class="match-details teams"]/p/span[@class="team-away teams"]/a/text()').extract()[0]
+            game['score'] = sel.xpath('td[@class="match-details teams"]/p/span[@class="score"]/abbr/text()').extract()[0]
+            
+            competition = sel.xpath('td[@class="match-competition"]/text()').extract()[0]
+            if sel.xpath('td[@class="match-competition"]/span'):
+                competition = competition + sel.xpath('td[@class="match-competition"]/span/text()').extract()[0]
+            game['competition'] = competition
+            
+            yield game
+
+class fixturesSpider(scrapy.Spider):
+    name = "fixtures"
+    allowed_domains = ["bbc.com"]
+    start_urls = ["http://www.bbc.com/sport/football/teams/{0}/fixtures".format(TEAM_NAME)]
+    
+    def parse(self, response):
+        for sel in response.xpath('//tr[@class="preview"]'):
+            game = fixturesItem()
+            game['date'] = sel.xpath('td[@class="match-date"]/text()').extract()[0]
+            game['homeTeam'] = sel.xpath('td[@class="match-details teams"]/p/span[@class="team-home teams"]/a/text()').extract()[0]
+            game['awayTeam'] = sel.xpath('td[@class="match-details teams"]/p/span[@class="team-away teams"]/a/text()').extract()[0]
+            
+            competition = sel.xpath('td[@class="match-competition"]/text()').extract()[0]
+            if sel.xpath('td[@class="match-competition"]/span'):
+                competition = competition + sel.xpath('td[@class="match-competition"]/span/text()').extract()[0]
+            game['competition'] = competition
+    
+            yield game
+
+class ranksSpider(scrapy.Spider):
+    name = "ranks"
+    allowed_domains = ["bbc.com"]
+    start_urls = ["http://www.bbc.com/sport/football/{0}/table".format(LEAGUE_NAME)]
+    
+    def parse(self, response):
+        for sel in response.xpath('//table[@class="table-stats"]/tbody/tr'):
+            team = ranksItem()
+            team['rank'] = sel.xpath('td[@class="position"]/span[@class="position-number"]/text()').extract()[0]
+            team['teamName'] = sel.xpath('td[@class="team-name"]/a/text()').extract()[0]
+            team['points'] = sel.xpath('td[@class="points"]/text()').extract()[0]
+            team['wins'] = sel.xpath('td[@class="won"]/span/text()').extract()[0]
+            team['draws'] = sel.xpath('td[@class="drawn"]/text()').extract()[0]
+            team['losses'] = sel.xpath('td[@class="lost"]/text()').extract()[0]
+            
+            yield team
+
